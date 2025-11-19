6052f6bf12c81641904be6d3101a19e56704094f
foryourfreedom2023 <foryourfreedom2023@naver.com>
2025-11-19 22:57:09 +0900

    Updates image domains for backend storage
    Updates the allowed image domains in the Next.js configuration to include the new backend storage domain and localhost. This ensures that images from the new storage location can be properly displayed.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const { i18n } = require('./next-i18next.config');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['127.0.0.1', 'single-fire.s3.ap-northeast-2.amazonaws.com', '192.168.0.145'],
+    domains: ['millmus-global-backend-storage.s3.ap-northeast-2.amazonaws.com', '127.0.0.1', 'localhost', 'single-fire.s3.ap-northeast-2.amazonaws.com', '192.168.0.145'],
   },
   i18n,
 };
